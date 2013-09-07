b9d5599dbc8298e366f54a348b4f00a58af7a90c
Jason Goh <jasongoh@MacBook-Air.local>
2013-09-07 22:47:43 +0800

    Added voice recognition

--- VideoSubtitling/src/com/example/videosubtitling/MainActivity.java
+++ VideoSubtitling/src/com/example/videosubtitling/MainActivity.java
@@ -1,55 +1,199 @@
 package com.example.videosubtitling;
 
 import java.io.IOException;
+
 import android.app.Activity;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends Activity
-{
+import com.example.videosubtitling.speechtotext.AppInfo;
+import com.nuance.nmdp.speechkit.Prompt;
+import com.nuance.nmdp.speechkit.Recognition;
+import com.nuance.nmdp.speechkit.Recognizer;
+import com.nuance.nmdp.speechkit.SpeechError;
+import com.nuance.nmdp.speechkit.SpeechKit;
+
+public class MainActivity extends Activity {
 	private static final String TAG = "AudioRecordTest";
 	private static final String AUDIO_FILE_NAME = "/audiorecordtest.3gp";
+
+	private static final int LISTENING_DIALOG = 0;
+	private Recognizer mCurrentRecognizer;
+	private Recognizer.Listener mRecognizerListener;
+	private Handler mHandler;
 
 	private static String mFileName = null;
 	private MediaRecorder mRecorder = null;
 	private MediaPlayer mPlayer = null;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	private static SpeechKit sSpeechKit;
 
-		// Set recording output path
-		mFileName = Environment.getExternalStorageDirectory().getAbsolutePath()
-		    + AUDIO_FILE_NAME;
+	// Allow other activities to access the SpeechKit instance.
+	static SpeechKit getSpeechKit() {
+		return sSpeechKit;
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		setVolumeControlStream(AudioManager.STREAM_MUSIC); // So that the 'Media
+		// Volume' applies
+		// to this activity
+
+		mHandler = new Handler();
+		mRecognizerListener = createListener();
+
+		// If this Activity is being recreated due to a config change (e.g.
+		// screen rotation), check for the saved SpeechKit instance.
+		sSpeechKit = (SpeechKit) getLastNonConfigurationInstance();
+		if (sSpeechKit == null) {
+			sSpeechKit = SpeechKit.initialize(getApplication()
+					.getApplicationContext(), AppInfo.SpeechKitAppId,
+					AppInfo.SpeechKitServer, AppInfo.SpeechKitPort,
+					AppInfo.SpeechKitSsl, AppInfo.SpeechKitApplicationKey);
+			sSpeechKit.connect();
+			// TODO: Keep an eye out for audio prompts not working on the Droid
+			// 2 or other 2.2 devices.
+			Prompt beep = sSpeechKit
+					.defineAudioPrompt(com.example.videosubtitling.R.raw.beep);
+			sSpeechKit.setDefaultRecognizerPrompts(beep, Prompt.vibration(100),
+					null, null);
+		}
+
+		// Set recording output path
+		mFileName = Environment.getExternalStorageDirectory().getAbsolutePath()
+				+ AUDIO_FILE_NAME;
+	}
+
+	private void startRecognizeSpeech() {
+		setResults(new Recognition.Result[0]);
+
+		// if (v == dictationButton) {
+		mCurrentRecognizer = getSpeechKit().createRecognizer(
+				Recognizer.RecognizerType.Dictation,
+				Recognizer.EndOfSpeechDetection.Long, "en_US",
+				mRecognizerListener, mHandler);
+		// } else {
+		// mCurrentRecognizer = getSpeechKit().createRecognizer(
+		// Recognizer.RecognizerType.Search,
+		// Recognizer.EndOfSpeechDetection.Short, "en_US",
+		// mRecognizerListener, mHandler);
+		// }
+		mCurrentRecognizer.start();
+	}
+
+	private void stopRecognizeSpeech() {
+		mCurrentRecognizer.stopRecording();
+	}
+
+	private Recognizer.Listener createListener() {
+		return new Recognizer.Listener() {
+			@Override
+			public void onRecordingBegin(Recognizer recognizer) {
+
+				// Create a repeating task to update the audio level
+				Runnable r = new Runnable() {
+					public void run() {
+						if (mCurrentRecognizer != null) {
+							mHandler.postDelayed(this, 500);
+						}
+					}
+				};
+				r.run();
+			}
+
+			@Override
+			public void onRecordingDone(Recognizer recognizer) {
+
+			}
+
+			@Override
+			public void onError(Recognizer recognizer, SpeechError error) {
+				if (recognizer != mCurrentRecognizer)
+					return;
+
+				mCurrentRecognizer = null;
+
+				// Display the error + suggestion in the edit box
+				String detail = error.getErrorDetail();
+				String suggestion = error.getSuggestion();
+
+				if (suggestion == null)
+					suggestion = "";
+				setResult(detail + "\n" + suggestion);
+				// for debugging purpose: printing out the speechkit session id
+				android.util.Log.d("Nuance SampleVoiceApp",
+						"Recognizer.Listener.onError: session id ["
+								+ getSpeechKit().getSessionId() + "]");
+			}
+
+			@Override
+			public void onResults(Recognizer recognizer, Recognition results) {
+
+				mCurrentRecognizer = null;
+				int count = results.getResultCount();
+				Recognition.Result[] rs = new Recognition.Result[count];
+				for (int i = 0; i < count; i++) {
+					rs[i] = results.getResult(i);
+				}
+				setResults(rs);
+				// for debugging purpose: printing out the speechkit session id
+				android.util.Log.d("Nuance SampleVoiceApp",
+						"Recognizer.Listener.onResults: session id ["
+								+ getSpeechKit().getSessionId() + "]");
+			}
+		};
+	}
+
+	private void setResult(String result) {
+		TextView t = (TextView) findViewById(R.id.translated_text);
+		if (t != null)
+			t.setText(result);
+	}
+
+	private void setResults(Recognition.Result[] results) {
+		if (results.length > 0) {
+			setResult(results[0].getText());
+		} else {
+			setResult("");
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (sSpeechKit != null) {
+			sSpeechKit.release();
+			sSpeechKit = null;
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
-	public void onPause()
-	{
+	public void onPause() {
 		super.onPause();
-		if (mRecorder != null)
-		{
+		if (mRecorder != null) {
 			mRecorder.release();
 			mRecorder = null;
 		}
 
-		if (mPlayer != null)
-		{
+		if (mPlayer != null) {
 			mPlayer.release();
 			mPlayer = null;
 		}
@@ -58,16 +202,16 @@ public class MainActivity extends Activity
 	/**
 	 * Method to handle respective button clicks.
 	 */
-	public void onClick(View view)
-	{
-		switch (view.getId())
-		{
+	public void onClick(View view) {
+		switch (view.getId()) {
 		case R.id.start_recording:
-			startAudioRecording();
+			// startAudioRecording();
+			startRecognizeSpeech();
 			break;
 
 		case R.id.stop_recording:
-			stopAudioRecording();
+			// stopAudioRecording();
+			stopRecognizeSpeech();
 			break;
 
 		case R.id.start_playing:
@@ -86,20 +230,16 @@ public class MainActivity extends Activity
 	/**
 	 * Start audio recording
 	 */
-	private void startAudioRecording()
-	{
+	private void startAudioRecording() {
 		mRecorder = new MediaRecorder();
 		mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
 		mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
 		mRecorder.setOutputFile(mFileName);
 		mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
-		try
-		{
+		try {
 			mRecorder.prepare();
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			Log.e(TAG, "prepare() failed");
 		}
 
@@ -109,8 +249,7 @@ public class MainActivity extends Activity
 	/**
 	 * Stop audio recording
 	 */
-	private void stopAudioRecording()
-	{
+	private void stopAudioRecording() {
 		mRecorder.stop();
 		mRecorder.release();
 		mRecorder = null;
@@ -119,17 +258,13 @@ public class MainActivity extends Activity
 	/**
 	 * Start audio playback
 	 */
-	private void startAudioPlayback()
-	{
+	private void startAudioPlayback() {
 		mPlayer = new MediaPlayer();
-		try
-		{
+		try {
 			mPlayer.setDataSource(mFileName);
 			mPlayer.prepare();
 			mPlayer.start();
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			Log.e(TAG, "prepare() failed");
 		}
 	}
@@ -137,9 +272,17 @@ public class MainActivity extends Activity
 	/**
 	 * Stop audio playback
 	 */
-	private void stopAudioPlayback()
-	{
+	private void stopAudioPlayback() {
 		mPlayer.release();
 		mPlayer = null;
+	}
+
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		// Save the SpeechKit instance, because we know the Activity will be
+		// immediately recreated.
+		SpeechKit sk = sSpeechKit;
+		sSpeechKit = null; // Prevent onDestroy() from releasing SpeechKit
+		return sk;
 	}
 }
