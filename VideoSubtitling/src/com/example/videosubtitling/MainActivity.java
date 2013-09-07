03b01b975f480cf85d72c63f956a533e4bb3ea37
unknown <hui-joo.goh@LTKL0002.ad.avira.com>
2013-09-07 21:58:56 +0800

    Added video implementation. Note: Landscape not supported yet!

--- VideoSubtitling/src/com/example/videosubtitling/MainActivity.java
+++ VideoSubtitling/src/com/example/videosubtitling/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.videosubtitling;
 
+import java.io.File;
 import java.io.IOException;
 import android.app.Activity;
+import android.graphics.Path;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
 import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.VideoView;
 
 public class MainActivity extends Activity
 {
 	private static final String TAG = "AudioRecordTest";
-	private static final String AUDIO_FILE_NAME = "/audiorecordtest.3gp";
+	private static final String ENVIRONMENT_PATH = Environment
+	    .getExternalStorageDirectory().getAbsolutePath() + File.separator;
+	private static final String AUDIO_FILE_NAME = "audiorecordtest.3gp";
 
-	private static String mFileName = null;
+	private String mVideoFileName = "vid.mp4"; // TODO: allow user to specify
+	                                           // the video
+	private String mFileName = null;
 	private MediaRecorder mRecorder = null;
 	private MediaPlayer mPlayer = null;
+	private VideoView mVideoView;
+	private ProgressBar mProgressBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -25,9 +39,26 @@ public class MainActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		mVideoView = (VideoView) findViewById(R.id.video_view);
+		mProgressBar = (ProgressBar) findViewById(R.id.video_progress_bar);
+
+		mVideoView.setOnTouchListener(new View.OnTouchListener()
+		{
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event)
+			{
+				onVideoTouch();
+				return false;
+			}
+		});
+
 		// Set recording output path
-		mFileName = Environment.getExternalStorageDirectory().getAbsolutePath()
-		    + AUDIO_FILE_NAME;
+		mFileName = ENVIRONMENT_PATH + AUDIO_FILE_NAME;
+
+		// Play video
+		String videoPath = ENVIRONMENT_PATH + mVideoFileName;
+		new PlayVideoAsyncTask(videoPath).execute();
 	}
 
 	@Override
@@ -58,7 +89,7 @@ public class MainActivity extends Activity
 	/**
 	 * Method to handle respective button clicks.
 	 */
-	public void onClick(View view)
+	public void onButtonClick(View view)
 	{
 		switch (view.getId())
 		{
@@ -80,6 +111,21 @@ public class MainActivity extends Activity
 
 		default:
 			break;
+		}
+	}
+
+	/**
+	 * Method to handle video's behavior (pause/resume video) upon user touch.
+	 */
+	private void onVideoTouch()
+	{
+		if (mVideoView.isPlaying())
+		{
+			mVideoView.pause();
+		}
+		else
+		{
+			mVideoView.start();
 		}
 	}
 
@@ -141,5 +187,65 @@ public class MainActivity extends Activity
 	{
 		mPlayer.release();
 		mPlayer = null;
+	}
+
+	/**
+	 * Class to play the video based on the path passed in.
+	 */
+	private class PlayVideoAsyncTask extends AsyncTask<Void, Integer, Void>
+	{
+		int mDuration = 0;
+		int mCurrent = 0;
+
+		PlayVideoAsyncTask(String videoPath)
+		{
+			mVideoView.setVideoURI(Uri.parse(videoPath));
+
+			mProgressBar.setProgress(0);
+			mProgressBar.setMax(100);
+		}
+
+		@Override
+		protected Void doInBackground(Void... params)
+		{
+
+			mVideoView.start();
+			mVideoView.setOnPreparedListener(new OnPreparedListener()
+			{
+
+				public void onPrepared(MediaPlayer mp)
+				{
+					mDuration = mVideoView.getDuration();
+				}
+			});
+
+			do
+			{
+				mCurrent = mVideoView.getCurrentPosition();
+				System.out.println("duration - " + mDuration + " current- "
+				    + mCurrent);
+				try
+				{
+					publishProgress((int) (mCurrent * 100 / mDuration));
+					if (mProgressBar.getProgress() >= 100)
+					{
+						break;
+					}
+				}
+				catch (Exception e)
+				{
+				}
+			} while (mProgressBar.getProgress() <= 100);
+
+			return null;
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... values)
+		{
+			super.onProgressUpdate(values);
+			System.out.println(values[0]);
+			mProgressBar.setProgress(values[0]);
+		}
 	}
 }
